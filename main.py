3fb15f3876c019c036864d818fa1354f17797fd0
Lopez <glopez810@gmail.com>
2022-07-25 21:11:51 -0500

    added deal card function for user and computer.

--- main.py
+++ main.py
@@ -33,11 +33,27 @@
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
+def deal_card():
+    """Returns random card"""
+    random_card = random.choice(cards)
+
+    return random_card
 
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+print(user_cards)
+print(computer_cards)
 
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input
 #and returns the score.
